9625fc5192c53f9493307fb29f1eef1e10880871
Edie Lemoine <e.lemoine@outlook.com>
2017-12-13 12:08:25 +0100

    Added better conditions handling; negative, arrays and comma separated

--- assets/js/admin/ep-admin-jquery.js
+++ assets/js/admin/ep-admin-jquery.js
@@ -50,25 +50,51 @@ function conditionCheck() {
         conditions.push( this.split( '=' ) );
       }
     });
-    // console.log(conditions);
+
     bool = [];
+
     $( conditions ).each( function() {
-      bool.push( $( "#" + this[0] ).val() === this[1] );
+      value = this[1];
+      element = "#" + this[0];
+
+      // In case it's a checkbox or radio
+      if ( ! $( element ).val() ) {
+        // In case there are multiple values required
+        if ( value.indexOf( ',' ) >= 0 ) {
+          values = value.split( ',' );
+          $(values).each(function() {
+            // $.trim in case values are comma separated with spaces
+            bool.push( $( element ).find( 'input[name$="['+$.trim(this)+']"]' ).attr( 'checked' ) );
+          });
+        }
+        else {
+          bool.push( $( element ).find( 'input[name$="['+value+']"]' ).attr( 'checked' ) );
+        }
+      }
+      // All other cases
+      else {
+        // Handle negative conditions
+        if ( value.indexOf( '!' ) >= 0 ) {
+          bool.push( $( element ).val() !== value.replace( '!', '' ) );
+        }
+        else {
+          bool.push( $( element ).val() === value );
+        }
+      }
     });
 
-    bool = allTrue(bool);
-
-    epSetVisibility( parent, bool );
+    // Hide or show element based on whether all conditions are met or not
+    epSetVisibility( parent, allTrue( bool ) );
   });
 };
 
 function epSetVisibility( parent, bool ) {
   if ( bool ) {
-    $(parent).closest('tr').fadeIn('fast').addClass('yes');
+    $(parent).closest('tr').fadeIn('fast');
     $(parent).closest('input, select, textarea').prop('disabled', false);
   }
   else {
-    $(parent).closest('tr').fadeOut('fast').addClass('no');
+    $(parent).closest('tr').fadeOut('fast');
     $(parent).closest('input, select, textarea').prop('disabled', true);
   }
 }
